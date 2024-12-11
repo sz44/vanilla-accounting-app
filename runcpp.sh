e11330fe386bb46fb01ed9981a5315ed8653a156
sz44 <semion44@gmail.com>
2024-12-11 11:37:10 -0800

    update runcpp.sh

--- runcpp.sh
+++ runcpp.sh
@@ -1,1 +1,9 @@
-emcc -O3 -lembind -o code.js ./c/code.cpp -sALLOW_MEMORY_GROWTH=1 -sASSERTIONS=1
+# emcc -O3 -lembind -o code.js ./c/code.cpp -sALLOW_MEMORY_GROWTH=1 -sASSERTIONS=1
+
+emcc -lembind ./c/code.cpp  \
+	-o gluecode.js \
+	-s ENVIRONMENT=web \
+	-s MODULARIZE=1 \
+	-s EXPORT_ES6=1 \
+	-s SINGLE_FILE=1 \
+	-s ALLOW_MEMORY_GROWTH=1
